c17e1d65b2515219a12bd08796cfbed491d164e0
Stephen <stepanfilonov@gmail.com>
2018-11-27 15:00:57 +0700

    Removed Circle Detection, code optimization, more comments

--- track.py
+++ track.py
@@ -4,8 +4,6 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QPixmap, QImage
-from functools import reduce
-from collections import deque
 import cv2
 import numpy as np
 
@@ -19,19 +17,22 @@ class Window(QMainWindow):
         self.image = None
         self.cameraRuns = False
         self.pageTwo = False
-        self.showPupils = False
         self.load_cascades()
         self.load_blob_detector()
         self.startButton.clicked.connect(self.start_webcam)
         self.stopButton.clicked.connect(self.stop_webcam)
         self.nextButton.clicked.connect(self.next_page)
-        self.coordsQueueLeft = deque()
-        self.coordsQueueRight = deque()
+        self.oldRight = None
+        self.oldLeft = None
+        self.oldRightArea = None
+        self.oldLeftArea = None
 
     def start_webcam(self):
         if not self.cameraRuns:
             self.capture = cv2.VideoCapture(cv2.CAP_DSHOW)  # VideoCapture(0) drops error# -1072875772
-            self.cameraRuns = not self.cameraRuns
+            if self.capture is None:
+                self.capture = cv2.VideoCapture(0)
+            self.cameraRuns = True
             self.timer = QTimer(self)
             self.timer.timeout.connect(self.update_frame)
             self.timer.start(2)
@@ -41,70 +42,48 @@ class Window(QMainWindow):
 
     def update_frame(self):
         _, self.bImage = self.capture.read()
-        if not self.showPupils:
+        if not self.pupilsCheckbox.isChecked():
             self.display_image(self.bImage, 1)
         if not self.pageTwo:  # first screen
             self.pImage = cv2.cvtColor(self.bImage, cv2.COLOR_RGB2GRAY)
-            self.param1 = self.param1Slider.value()
-            self.param2 = self.param2Slider.value()
-            self.minRadius = self.minRadiusSlider.value()
-            self.maxRadius = self.maxRadiusSlider.value()
-            self.arg1 = self.arg1Slider.value()
-            self.arg2 = self.arg2Slider.value()
-            self.threshold = self.thresholdSlider.value()
+            self.rightEyeThreshold = self.rightEyeThresholdSlider.value()
+            self.leftEyeThreshold = self.leftEyeThresholdSlider.value()
             oldLeft = None
             oldRight = None
             face_frame, face_frame_gray, lest, rest, faceX, faceY = self.detect_face(self.bImage, self.pImage)
             if face_frame is not None:
                 self.leyeframe, self.reyeframe, self.leyeframeG, self.reyeframeG = self.detect_eyes(face_frame, face_frame_gray, lest, rest)
-                if self.leyeframe is not None:
-                    if self.leftEyeCheckbox.isChecked():
-                        if self.blobToggle.value():
-                            lheight, lwidth = self.leyeframe.shape[:2]
-                            self.leyeframeG = self.leyeframeG[15:lheight, 0:lwidth]  # cut eyebrows out
-                            self.leyeframe = self.leyeframe[15:lheight, 0:lwidth]
-                            self.lkeypoints = self.process_eye(self.leyeframeG)
-                            if self.lkeypoints is not None:
-                                cv2.drawKeypoints(self.leyeframe, self.lkeypoints, self.leyeframe, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                                oldLeft = self.lkeypoints
-                            else:
-                                cv2.drawKeypoints(self.leyeframe, oldLeft, self.leyeframe, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                if self.leyeframe is not None:  # displays left eye
+                    if self.leftEyeCheckbox.isChecked():  # draws a circle on left eye pupil
+                        lheight, lwidth = self.leyeframe.shape[:2]
+                        self.leyeframeG = self.leyeframeG[15:lheight, 0:lwidth]  # cut eyebrows out (15 px)
+                        self.leyeframe = self.leyeframe[15:lheight, 0:lwidth]
+                        self.lkeypoints = self.process_eye(self.leyeframeG, self.leftEyeThreshold)
+                        if self.lkeypoints is not None:
+                            cv2.drawKeypoints(self.leyeframe, self.lkeypoints, self.leyeframe, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                            oldLeft = self.lkeypoints
                         else:
-                            lIris = self.process_eye1(self.leyeframeG)
-                            if lIris is not None:
-                                self.lx, self.ly, self.lradius = self.stabilizeLeft(lIris[0], lIris[1], lIris[2])
-                                cv2.circle(self.leyeframe, (self.lx, self.ly), self.lradius, (0, 255, 0), 2)
-                                cv2.circle(self.leyeframe, (self.lx, self.ly), 2, (0, 0, 255), 3)
+                            cv2.drawKeypoints(self.leyeframe, oldLeft, self.leyeframe, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
                     self.leyeframe = np.require(self.leyeframe, np.uint8, 'C')
                     self.display_image(self.leyeframe, 3)
-                if self.reyeframe is not None:
-                    if self.rightEyeCheckbox.isChecked():
-                        if self.blobToggle.value():
-                            rheight, rwidth = self.reyeframe.shape[:2]
-                            self.reyeframeG = self.reyeframeG[15:rheight, 0:rwidth]
-                            self.reyeframe = self.reyeframe[15:rheight, 0:rwidth]
-                            self.rkeypoints = self.process_eye(self.reyeframeG)
-                            if self.rkeypoints is not None:
-                                cv2.drawKeypoints(self.reyeframe, self.rkeypoints, self.reyeframe, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                                oldRight = self.rkeypoints
-                            else:
-                                cv2.drawKeypoints(self.reyeframe, oldRight, self.reyeframe, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                        else:
-                            rIris = self.process_eye1(self.reyeframeG)
-                            if rIris is not None:
-                                self.x, self.y, self.radius = self.stabilizeRight(rIris[0], rIris[1], rIris[2])
-                                cv2.circle(self.reyeframe, (self.x, self.y), self.radius, (0, 255, 0), 2)
-                                cv2.circle(self.reyeframe, (self.x, self.y), 2, (0, 0, 255), 3)
+                if self.reyeframe is not None:  # displays right eye
+                    if self.rightEyeCheckbox.isChecked():  # draws a circle on right eye pupil
+                        rheight, rwidth = self.reyeframe.shape[:2]
+                        self.reyeframeG = self.reyeframeG[15:rheight, 0:rwidth] # cur eyebrows out (15 px)
+                        self.reyeframe = self.reyeframe[15:rheight, 0:rwidth]
+                        self.rkeypoints = self.process_eye(self.reyeframeG, self.rightEyeThreshold) # get blobs
+                        self.draw_blobs(self.reyeframe, self.rkeypoints, side="right") # draw blobs
+
                     self.reyeframe = np.require(self.reyeframe, np.uint8, 'C')
                     self.display_image(self.reyeframe, 4)
-                if self.pupilsCheckbox.isChecked():
-                    self.display_image(self.bImage, 1)
+            if self.pupilsCheckbox.isChecked():  # draws keypoints on pupils on main window
+                self.display_image(self.bImage, 1)
 
         else:  # second screen
             pass
 
     def display_image(self, img, window):
-        # to do: Grayscale display
+        # Makes OpenCV images displayable on PyQT, displays them
         qformat = QImage.Format_Indexed8
         if len(img.shape) == 3:
             if img.shape[2] == 4:  # RGBA
@@ -114,13 +93,13 @@ class Window(QMainWindow):
 
         outImage = QImage(img, img.shape[1], img.shape[0], img.strides[0], qformat)  # BGR to RGB
         outImage = outImage.rgbSwapped()
-        if window == 1:
+        if window == 1:  # main window
             self.baseImage.setPixmap(QPixmap.fromImage(outImage))
             self.baseImage.setScaledContents(True)
-        if window == 3:
+        if window == 3:  # left eye window
             self.leftEyeBox.setPixmap(QPixmap.fromImage(outImage))
             self.leftEyeBox.setScaledContents(True)
-        if window == 4:
+        if window == 4:  # right eye window
             self.rightEyeBox.setPixmap(QPixmap.fromImage(outImage))
             self.rightEyeBox.setScaledContents(True)
 
@@ -212,8 +191,8 @@ class Window(QMainWindow):
                     pass  # nostril
         return leftEye, rightEye, leftEyeG, rightEyeG
 
-    def process_eye(self, img):  # blob processing
-        _, img = cv2.threshold(img, self.threshold, 255, cv2.THRESH_BINARY)
+    def process_eye(self, img, threshold):  # blob processing
+        _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
         img = cv2.erode(img, None, iterations=2)
         img = cv2.dilate(img, None, iterations=4)
         img = cv2.medianBlur(img, 5)
@@ -221,58 +200,37 @@ class Window(QMainWindow):
 
         return keypoints
 
-    def process_eye1(self, img):  # circular processing
-        # kernel_sharpening = np.array([[-1, -1, -1], #maybe use later as an alternative to equalizeHist()
-        #                               [-1, 9, -1],
-        #                               [-1, -1, -1]])
-        # sharpened = cv2.filter2D(img, -1, kernel_sharpening)
-        equ = cv2.equalizeHist(img)
-        circles = cv2.HoughCircles(equ, cv2.HOUGH_GRADIENT, self.arg1, self.arg2,
-                                   param1=self.param1, param2=self.param2, minRadius=self.minRadius, maxRadius=self.maxRadius)
-        smallest = 255
-        if circles is None or circles[0, 0, 1] == 0 or circles[0, 0, 0] == 0:  # openCV bug, returns [0, 0, 0] instead of None when nothing is detected (sometimes)
-            return None
-        for i in circles[0, :]:  # filter the "blackest" circle out by comparing average pixel value in the circles
-            X = int(i[0])
-            Y = int(i[1])
-            r = i[2]
-            a = int(r / 1.7)  # pythagoras theorem (sqrt2)
-            total = 0
-            count = 0
-            for y in range(Y - 2 * a, Y):
-                for x in range(X, X + 2 * a):
-                    if x < img.shape[1] and y < img.shape[0]:  # img.shape returns (Y, X), not (X, Y) lol
-                        pixel = equ.item(x, y)
-                        total += pixel
-                        count += 1
-            avg = total / count
-            if avg < smallest:
-                smallest = avg
-                iris = i
-                # circles = np.uint16(np.around(circles))
-        return iris
+    def draw_blobs(self, img, keypoints, side="right"):
+        """Draws blobs, uses a kwarg to determine which eye to draw (left or right)"""
+        print(keypoints, self.oldRight, self.oldLeft)
+        if len(keypoints) == 1:  # when one blob is found, we draw it and remember its position, area
+            cv2.drawKeypoints(img, keypoints, img, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            if side == "right":
+                self.oldRight = keypoints
+                self.oldRightArea = keypoints[0].size
+            else:
+                self.oldLeft = keypoints
+                self.oldLeftArea = keypoints
+        elif len(keypoints) == 0 and self.oldRight is not None and side == "right":  # when no blobs are found, we draw the last remembered blob(right)
+            cv2.drawKeypoints(img, self.oldRight, img, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    def stabilizeLeft(self, x, y, r):  # averages the last five X, Y and radius values with help of dequeue
-        if len(self.coordsQueueLeft) == 5:
-            self.coordsQueueLeft.popleft()
-        else:
-            for i in range(0, 5 - len(self.coordsQueueLeft)):
-                self.coordsQueueLeft.append((x, y, r))
-        stableX = reduce((lambda x, y: x + y), [iris[0] for iris in self.coordsQueueLeft]) / 5
-        stableY = reduce((lambda x, y: x + y), [iris[1] for iris in self.coordsQueueLeft]) / 5
-        stableRadius = reduce((lambda x, y: x + y), [iris[2] for iris in self.coordsQueueLeft]) / 5
-        return int(stableX), int(stableY), int(stableRadius)
+        elif len(keypoints) == 0 and self.oldRight is not None and side == "left": # when no blobs are found, we draw the last remembered blob(left)
+            cv2.drawKeypoints(img, self.oldLeft, img, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    def stabilizeRight(self, x, y, r):  # averages the last five X, Y and radius values with help of dequeue
-        if len(self.coordsQueueRight) == 5:
-            self.coordsQueueRight.popleft()
-        else:
-            for i in range(0, 5 - len(self.coordsQueueRight)):
-                self.coordsQueueRight.append((x, y, r))
-        stableX = reduce((lambda x, y: x + y), [iris[0] for iris in self.coordsQueueRight]) / 5
-        stableY = reduce((lambda x, y: x + y), [iris[1] for iris in self.coordsQueueRight]) / 5
-        stableRadius = reduce((lambda x, y: x + y), [iris[2] for iris in self.coordsQueueRight]) / 5
-        return int(stableX), int(stableY), int(stableRadius)
+        elif len(keypoints) > 1:  # when multiple blobs are found the one whose area is the closest to the last detected blob's area is drawn
+            tmp = 1000
+            if side == "right":
+                area = self.oldRightArea
+            else:
+                area = self.oldLeftArea
+            for keypoint in keypoints: # filter out odd blobs
+                if abs(keypoint.size - area) < tmp:
+                    keypoint_to_draw = np.array(keypoint)
+                    tmp = abs(keypoint.size - area)
+            cv2.drawKeypoints(img, keypoint_to_draw, img, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    def process_pupil(self, img):  # pupil processing for conreal reflection
+        pass
 
 
 if __name__ == "__main__":
