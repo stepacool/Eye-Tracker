408247d91b6193d5e299b3a0e1a42d555763a57a
Stepan <stepanfilonov@gmail.com>
2021-10-30 20:02:20 +0300

    Allow to specify camera_id in args, support camera_id in VideoCapturer class

--- project/main.py
+++ project/main.py
@@ -27,6 +27,15 @@ def get_args():
         dest="frame_source",
         choices=FRAME_SOURCES.keys(),
         default="camera",
+        help="What should be the frames source. A video/file, a folder with frames or a camera"
+    )
+    parser.add_argument(
+        "-cam-id",
+        "--camera-id",
+        action="store",
+        dest="camera_id",
+        choices=FRAME_SOURCES.keys(),
+        help="If your camera has an unusual ID in the system, pass it in this argument. Use only if your frame-source is camera(default)"
     )
 
     args = parser.parse_args()
@@ -36,13 +45,17 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    frame_source = FRAME_SOURCES[args.frame_source]()
+    frame_source = FRAME_SOURCES[args.frame_source]
+
+    frames_source_init_kwargs = {}
+    if args.camera_id and args.frame_source == "camera":
+        frames_source_init_kwargs["cam_id"] = args.camera_id
 
     capture = HaarCascadeBlobCapture()
 
     app = QApplication(sys.argv)
 
-    window = Window(frame_source, capture)
+    window = Window(frame_source(**frames_source_init_kwargs), capture)
     window.setWindowTitle("Eye Tracking")
     window.show()
     sys.exit(app.exec())
--- project/video_source.py
+++ project/video_source.py
@@ -21,18 +21,31 @@ class FrameSource(Protocol):
 
 
 class OpenCVCameraVideoSource:
-    def __init__(self):
+    def __init__(self, cam_id=None):
         self.camera_is_running = False
+        self.cam_id = cam_id
+        self.capture = None
+
+    def _check_camera(self):
+        return self.capture is not None and self.capture.read()[0]
 
     def start(self):
         if not self.camera_is_running:
-            for camera_device_index in range(0, 3000, 100):
-                self.capture = cv2.VideoCapture(
-                    camera_device_index
-                )  # VideoCapture(0) sometimes drops error #-1072875772
-                if self.capture is not None and self.capture.read()[0]:
+            if self.cam_id is not None:
+                self.capture = cv2.VideoCapture(self.cam_id)
+
+                if not self._check_camera():
+                    raise SystemError(f"Camera id={self.cam_id} not working")
+
+                self.camera_is_running = True
+                return
+            for camera_device_index in range(0, 5000, 100):  # Try different camera IDs, they usually increment by 100
+                self.capture = cv2.VideoCapture(camera_device_index)
+
+                if self._check_camera():
                     self.camera_is_running = True
-                    break
+                    return
+            raise SystemError("Couldn't find a camera on the device. shell \nls /dev/ | grep *video*\n might help")
 
     def stop(self):
         if self.camera_is_running:
